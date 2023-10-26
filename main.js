c1df31b80fad27a7c0aca8f245cf414519f911ce
Sulivan Souza <sulivanprenholato2012@gmail.com>
2023-10-26 14:22:14 -0300

    add exercicio_html_js

---
+++ main.js
@@ -0,0 +1,20 @@
+document
+  .getElementById("formulario")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const campoA = document.getElementById("campo-a").value;
+    const campoB = document.getElementById("campo-b").value;
+
+    if (campoB > campoA) {
+      document.getElementById("mensagem").textContent =
+        "Sucesso Campo B e Maior que Campo A";
+      document.getElementById("mensagem").style.color = "green";
+      document.getElementById("mensagem").style.display = "flex";
+    } else {
+      document.getElementById("mensagem").textContent =
+        "Erro Campo B deve ser Maior que Campo A";
+      document.getElementById("mensagem").style.color = "red";
+      document.getElementById("mensagem").style.display = "flex";
+    }
+  });
