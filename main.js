f16ee85eb40d367ed4f877cadb8716c4cec2c677
sulivan7 <devsulivan@gmail.com>
2025-03-11 14:49:40 -0300

    new fix

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ function Hb20 (rodas, motor, combustivel, portas, janelas, marca, ano) {
 
 const carroDaLaura = new Hb20(4, '1.6', 'gasolina', 4, 4, 'Hyundai', 2015);
 const carroDoJoao = new Hb20(4, '10', 'gasolina', 4, 4, 'Hyundai', 2018);
+const carroDoRicardo = new Hb20(4, '1.6', 'gasolina', 4, 4, 'Hyundai', 2019);
 
 function Moto (rodas, motor, combustivel, capacete,) {
     Veiculo.call(this, rodas, motor, combustivel);
@@ -38,6 +39,7 @@ function Biz (rodas, motor, combustivel, capacete, marca, ano) {
 
 const motoDoJoao = new Biz(2, '125cc', 'gasolina', 'fechado', 'Honda', 2018);
 const motoDaLarissa = new Biz(2, '125cc', 'gasolina', 'fechado', 'Honda', 2015);
+const motoDoJames = new Biz(2, '125cc', 'gasolina', 'fechado', 'Honda', 2019);
 
 function Pessoa (nome, idade) {
     this.nome = nome;
@@ -57,6 +59,8 @@ const laura = new Pessoa('Laura', 20);
 const joao = new Pessoa('João', 15);
 const larissa = new Pessoa('Larissa', 18);
 const pedro = new Pessoa('Pedro', 17);
+const james = new Pessoa('James', 19);
+const ricardo = new Pessoa('Ricardo', 21);
 
 function Cliente (nome, idade, veiculoDoCliente) {
     Pessoa.call(this, nome, idade);
@@ -65,8 +69,10 @@ function Cliente (nome, idade, veiculoDoCliente) {
 
 const clienteLaura = new Cliente(laura.nome, laura.idade, carroDaLaura);
 const clienteJoao = new Cliente(joao.nome, joao.idade, motoDoJoao);
-const clienteLarissa = new Cliente('Larissa', 18, motoDaLarissa);
-const clientePedro = new Cliente('Pedro', 17, carroDoJoao);
+const clienteLarissa = new Cliente(larissa.nome, larissa.idade, motoDaLarissa);
+const clientePedro = new Cliente(pedro.nome, pedro.idade, carroDoJoao);
+const clienteJames = new Cliente(james.nome, james.idade, motoDoJames);
+const clienteRicardo = new Cliente(ricardo.nome, ricardo.idade, carroDoRicardo);
 
 console.log(clienteLaura);
 carroDaLaura.ligar();
@@ -87,3 +93,13 @@ console.log(clientePedro);
 carroDoJoao.ligar();
 carroDoJoao.desligar();
 clientePedro.habilitacao();
+
+console.log(clienteJames);
+motoDoJames.ligar();
+motoDoJames.desligar();
+clienteJames.habilitacao();
+
+console.log(clienteRicardo);
+carroDoRicardo.ligar();
+carroDoRicardo.desligar();
+clienteRicardo.habilitacao();
